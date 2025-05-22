a3743f43ed77f7ec27986b4d0868106d8c1cd302
Roman Ivanov <ivanov.roman.alexeevich@gmail.com>
2025-05-22 16:09:38 +0300

    Expand publish/revoke events format

--- server/src/main/kotlin/org/octopusden/octopus/dms/event/PublishComponentVersionEvent.kt
+++ server/src/main/kotlin/org/octopusden/octopus/dms/event/PublishComponentVersionEvent.kt
@@ -1,9 +1,9 @@
 package org.octopusden.octopus.dms.event
 
-import org.octopusden.octopus.dms.client.common.dto.ArtifactShortDTO
+import org.octopusden.octopus.dms.client.common.dto.ArtifactFullDTO
+import org.octopusden.octopus.dms.client.common.dto.ComponentVersionFullDTO
 
 data class PublishComponentVersionEvent(
-    val component: String,
-    val version: String,
-    val artifacts: List<ArtifactShortDTO>
+    val componentVersion: ComponentVersionFullDTO,
+    val artifacts: Set<ArtifactFullDTO>
 ) : Event(EventType.PUBLISH_COMPONENT_VERSION)
