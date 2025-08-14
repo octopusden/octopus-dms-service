ed58d16d818089dd8597d8d1f2bd04082a513544
D-Zlobin <dmitry.s.zlobin@gmail.com>
2025-08-14 22:04:16 +0300

    typo fix

--- server/src/frontend/components/ComponentsTree/index.js
+++ server/src/frontend/components/ComponentsTree/index.js
@@ -9,9 +9,9 @@ import get from "lodash/get";
 
 const mapStateToProps = (state) => {
     const {
-        components, loadingComponents, loadingArtifacts, currentArtifacts, errorMessage, confirmation, showRc, searching, serachQuery
+        components, loadingComponents, loadingArtifacts, currentArtifacts, errorMessage, confirmation, showRc, searching, searchQuery
     } = state.components
-    return {components, loadingComponents, loadingArtifacts, currentArtifacts, errorMessage, confirmation, showRc, searching, serachQuery}
+    return {components, loadingComponents, loadingArtifacts, currentArtifacts, errorMessage, confirmation, showRc, searching, searchQuery}
 }
 
 const mapDispatchToProps = (dispatch) => {
