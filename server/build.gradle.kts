7e2c60722c7dedb6f627255a70f93411411e9cd5
Stanislav Grokhotov <51979340+StanislavGro@users.noreply.github.com>
2025-12-09 16:06:37 +0300

    Remove UI from dms-service (#68)
    * Remove UI from dms-service
    
    * Fixed bug with executing test on Win
    
    * Added comment into copyArtifactoryDump
    
    * Changed comment on copyArtifactoryDump
    
    * Applied changes from PR
    
    * Applied changes from PR 2
    
    * Applied changes from PR 3

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -15,7 +15,6 @@ plugins {
     id("org.jetbrains.kotlin.plugin.noarg")
     id("com.bmuschko.docker-spring-boot-application") version "9.4.0"
     id("com.avast.gradle.docker-compose") version "0.16.9"
-    id("com.github.node-gradle.node") version "7.0.2"
     id("org.octopusden.octopus.oc-template")
     `maven-publish`
 }
@@ -166,7 +165,12 @@ ocTemplate{
 }
 
 val copyArtifactoryDump = tasks.register<Exec>("copyArtifactoryDump") {
-    val localFile = layout.projectDirectory.dir("../test-common/src/main/artifactory/dump").asFile.absolutePath
+    val localFile = layout.projectDirectory
+        .dir("../test-common/src/main/artifactory/dump")
+        .asFile
+        .absolutePath
+        // oc treats text before colon as pod name, strip Windows drive letter
+        .substringAfter(":")
     commandLine("oc", "cp", localFile, "-n", "okdProject".getExt(),
         "${ocTemplate.getPod("artifactory")}:/")
     dependsOn("ocCreate")
@@ -258,25 +262,6 @@ tasks.withType<Test> {
 
 tasks.named("dockerBuildImage") {
     dependsOn("test")
-}
-
-val npmBuild = tasks.register<com.github.gradle.node.npm.task.NpmTask>("npmBuild") {
-    dependsOn("npmInstall")
-    npmCommand.set(listOf("run", "build"))
-}
-
-tasks.withType<ProcessResources> {
-    dependsOn(npmBuild)
-}
-
-node {
-    version.set("16.20.2")
-    download.set(true)
-    npmVersion.set("8.19.4")
-}
-
-tasks.getByName<Delete>("clean") {
-    this.delete.add("$projectDir/node_modules")
 }
 
 tasks.named<org.springframework.boot.gradle.tasks.run.BootRun>("bootRun") {
